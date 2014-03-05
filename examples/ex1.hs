26f1e0c163be2c247d13349b15b442e1720c5915
seagull.kamome@gmail.com <seagull.kamome@gmail.com>
2014-03-05 20:59:40 +0900

    Add example

---
+++ examples/ex1.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Control.Monad (when, unless)
+import Control.Concurrent (threadDelay)
+--import Data.ByteString.Char8
+import Graphics.Rendering.FTGL as FTGL
+import Graphics.UI.GLFW as GLFW
+import Graphics.Rendering.OpenGL as GL
+
+
+main :: IO ()
+main = do
+  GLFW.init >>= flip unless (fail "Failed to initialize GLFW")
+
+  fnt <- FTGL.createExtrudeFont "examples/gothic.ttf"
+  FTGL.setFontFaceSize fnt 7 7
+  FTGL.setFontDepth fnt 1.0
+
+  GLFW.windowHint $ GLFW.WindowHint'Resizable False
+  Just win <- GLFW.createWindow 640 480 "FTGL haskell - ex1" Nothing Nothing
+  GLFW.makeContextCurrent $ Just win
+
+  GLFW.swapInterval 1
+  GL.clearColor $= Color4 1.0 1.0 1.0 1.0
+  let loop = GLFW.windowShouldClose win >>= flip unless go
+      go = do
+        GL.clear [GL.ColorBuffer]
+        GL.loadIdentity
+        
+        GL.scale 0.01 0.01 (0.5 :: GLdouble)
+        GL.color $ Color4 1.0 0 0 (1.0 :: GLdouble)
+        FTGL.renderFont fnt "Hello, 日本語はすける" FTGL.All
+
+        GL.translate $ Vector3 0.0 7.0 (0.0 :: GLdouble)
+        FTGL.renderFont fnt "GLFW+FTGL" FTGL.All
+
+        GL.translate $ Vector3 (-100) 7.0 (0.0 :: GLdouble)
+        FTGL.renderFont fnt "文字列の左下が原点で、上はY+。横はX+方向。Z=0平面に描かれる？" FTGL.All
+
+        GLFW.swapBuffers win
+        --threadDelay 200
+        loop
+    in loop
+
+  GLFW.destroyWindow win
+
+  FTGL.destroyFont fnt
+  
+  GLFW.terminate
+  return ()
